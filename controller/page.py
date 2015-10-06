21cc73877551ce15962af31df7455ef528d779ee
Silver Bullet <dyflive@live.cn>
2015-10-06 19:53:42 +0800

    Special page list finished.

--- controller/page.py
+++ controller/page.py
@@ -1,8 +1,36 @@
 #coding=utf-8
 __author__='silver'
 from controller.base import BaseHandler
+from util.filemanager import getdocumentdetail
+from util.filemanager import getdocumentlist
+from util.mdparser import article
+import tornado.web
 
 class IndexPage(BaseHandler):
+    def get(self, *args, **kwargs):
+        self.redirect('/list.aspx')
+
+    def post(self):
+        self.get()
+
+class SpecialPageListHandler(BaseHandler):
+    def get(self, *args, **kwargs):
+        special_page_root_dir=self.opts.document_location+"/pages/"
+        try:
+            lst=getdocumentlist(special_page_root_dir, recursive=False)
+            if len(lst)==0:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            raise tornado.web.HTTPError(404, reason='No special pages.')
+        for i in range(len(lst)):
+            lst[i].update(article(lst[i]['path']).extract())
+        lst.sort(key=lambda i:i['title'])
+        self.render('list.htm', pagetitle="Some lonely pages.", articleInfo=lst, pagenum=1, max_page=1)
+
+    def post(self):
+        self.get()
+
+class SpecialPageHandler(BaseHandler):
     def get(self, *args, **kwargs):
         self.redirect('/list.aspx')
 
--- main.py
+++ main.py
@@ -38,10 +38,14 @@ setting = {
 # Route config
 application = tornado.web.Application([
     (r"^/$", "controller.page.IndexPage"),
+
     (r"^/article\.aspx/((?:[\w\-!():.,\[\]]|(?:%20))+)$", "controller.article.ArticleHandler"),
+
     (r"^/list\.aspx/*$", "controller.list.FirstPageHandler"),
     (r"^/list\.aspx/(\d+)$", "controller.list.PageHandler"),
- #   (r"^/page\.aspx/((?:[\w\-!():.,\[\]]|(?:%20))+)$", "controller.page.SpecialPageHandler")
+
+    (r"^/page\.aspx/*$", "controller.page.SpecialPageListHandler"),
+    (r"^/page\.aspx/((?:[\w\-!():.,\[\]]|(?:%20))+)$", "controller.page.SpecialPageHandler")
  #   (r"^/abc/.*$", "controller.testdemo.TestHandler")
 ], **setting)
 
