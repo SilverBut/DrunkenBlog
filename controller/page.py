378555fe937305a60fc3f1d4a13fce638b445e1e
Silver Bullet <dyflive@live.cn>
2015-10-06 20:19:26 +0800

    Finished new index page module. Modify documents/pages/index.md to modify index page.

--- controller/page.py
+++ controller/page.py
@@ -6,12 +6,6 @@ from util.filemanager import getdocumentlist
 from util.mdparser import article
 import tornado.web
 
-class IndexPage(BaseHandler):
-    def get(self, *args, **kwargs):
-        self.redirect('/list.aspx')
-
-    def post(self):
-        self.get()
 
 class SpecialPageListHandler(BaseHandler):
     def get(self, *args, **kwargs):
@@ -24,8 +18,9 @@ class SpecialPageListHandler(BaseHandler):
             raise tornado.web.HTTPError(404, reason='No special pages.')
         for i in range(len(lst)):
             lst[i].update(article(lst[i]['path']).extract())
+        lst[:] = [d for d in lst if d.get('filename') != 'index']
+
         lst.sort(key=lambda i:i['title'])
-        print(lst)
         self.render('list.htm', disphandler='page.aspx', pagetitle="Some lonely pages.", articleInfo=lst, pagenum=1, max_page=1)
 
     def post(self):
@@ -36,11 +31,21 @@ class SpecialPageHandler(BaseHandler):
         special_page_root_dir=self.opts.document_location+"/pages"
         try:
             fileinfo=getdocumentdetail(special_page_root_dir+'/'+filename+'.md')
+            if (filename == 'index') and (kwargs['flag']!=1):
+                raise FileNotFoundError
         except FileNotFoundError:
             raise tornado.web.HTTPError(404, reason='Non-existing article.')
         blog=article(fileinfo['path'])
         blog.render()
         self.render("article.htm", title=blog.info['title'], md_html=blog.html, articleInfo=blog.info)
+
+    def post(self):
+        self.get()
+
+
+class IndexPage(SpecialPageHandler):
+    def get(self, *args, **kwargs):
+        super(IndexPage, self).get(filename='index', flag=1)
 
     def post(self):
         self.get()
